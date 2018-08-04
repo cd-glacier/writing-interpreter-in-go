06c0ff307f1b52092d64c60ee7a4b122edafb5ca
g-hyoga <hyoga0216@gmail.com>
2018-08-04 21:54:26 +0900

    :bug:

---
+++ src/ast/expression.go
@@ -0,0 +1,22 @@
+package ast
+
+import "github.com/g-hyoga/writing-interpreter-in-go/src/token"
+
+// ExpressionStatement implements ast.Statement interface.
+type ExpressionStatement struct {
+	Token      token.Token
+	Expression Expression
+}
+
+func (es *ExpressionStatement) statementNode() {}
+
+func (es *ExpressionStatement) TokenLiteral() string {
+	return es.Token.Literal
+}
+
+func (es *ExpressionStatement) String() string {
+	if es.Expression != nil {
+		return es.Expression.String()
+	}
+	return ""
+}
---
+++ src/lexer/lexer_helper.go
@@ -0,0 +1,17 @@
+package lexer
+
+import "github.com/g-hyoga/writing-interpreter-in-go/src/token"
+
+func newToken(tokenType token.TokenType, ch byte) token.Token {
+	return token.Token{Type: tokenType, Literal: string(ch)}
+}
+
+func isLetter(ch byte) bool {
+	return 'a' <= ch && ch <= 'z' ||
+		'A' <= ch && ch <= 'Z' ||
+		ch == '_'
+}
+
+func isDigit(ch byte) bool {
+	return '0' <= ch && ch <= '9'
+}
