6bd90534b458f1c82bd6e6a815498d218b251af2
g-hyoga <hyoga0216@gmail.com>
2018-08-04 22:13:44 +0900

    Improve parser dir

--- src/parser/parser.go
+++ src/parser/parser.go
@@ -1,8 +1,6 @@
 package parser
 
 import (
-	"fmt"
-
 	"github.com/g-hyoga/writing-interpreter-in-go/src/ast"
 	"github.com/g-hyoga/writing-interpreter-in-go/src/lexer"
 	"github.com/g-hyoga/writing-interpreter-in-go/src/logger"
@@ -41,21 +39,6 @@ func New(l *lexer.Lexer) *Parser {
 
 func (p *Parser) Errors() []string {
 	return p.errors
-}
-
-func (p *Parser) peekError(t token.TokenType) {
-	msg := fmt.Sprintf("expected next token to be %s, got %s instead", t, p.peekToken.Type)
-	p.errors = append(p.errors, msg)
-}
-
-func (p *Parser) nextToken() {
-	p.curToken = p.peekToken
-	p.peekToken = p.l.NextToken()
-
-	p.logger.WithFields(logrus.Fields{
-		"currentToken": p.curToken.Literal,
-		"peekToken":    p.peekToken.Literal,
-	}).Debug("[parser] nextToken")
 }
 
 func (p *Parser) ParseProgram() *ast.Program {
@@ -121,31 +104,6 @@ func (p *Parser) parseLetStatement() *ast.LetStatement {
 	}).Debug("[parser] parseLetStatement")
 
 	return stmt
-}
-
-func (p *Parser) curTokenIs(t token.TokenType) bool {
-	return p.curToken.Type == t
-}
-
-func (p *Parser) peekTokenIs(t token.TokenType) bool {
-	return p.peekToken.Type == t
-}
-
-func (p *Parser) expectPeek(t token.TokenType) bool {
-	if p.peekTokenIs(t) {
-		p.nextToken()
-		return true
-	} else {
-		return false
-	}
-}
-
-func (p *Parser) registerPrefix(tokenType token.TokenType, fn prefixParseFn) {
-	p.prefixParseFns[tokenType] = fn
-}
-
-func (p *Parser) registerInfix(tokenType token.TokenType, fn infixParseFn) {
-	p.infixParseFns[tokenType] = fn
 }
 
 func (p *Parser) parseReturnStatement() *ast.ReturnStatement {
---
+++ src/parser/parser_helper.go
@@ -0,0 +1,48 @@
+package parser
+
+import (
+	"fmt"
+
+	"github.com/g-hyoga/writing-interpreter-in-go/src/token"
+	"github.com/sirupsen/logrus"
+)
+
+func (p *Parser) peekError(t token.TokenType) {
+	msg := fmt.Sprintf("expected next token to be %s, got %s instead", t, p.peekToken.Type)
+	p.errors = append(p.errors, msg)
+}
+
+func (p *Parser) nextToken() {
+	p.curToken = p.peekToken
+	p.peekToken = p.l.NextToken()
+
+	p.logger.WithFields(logrus.Fields{
+		"currentToken": p.curToken.Literal,
+		"peekToken":    p.peekToken.Literal,
+	}).Debug("[parser] nextToken")
+}
+
+func (p *Parser) curTokenIs(t token.TokenType) bool {
+	return p.curToken.Type == t
+}
+
+func (p *Parser) peekTokenIs(t token.TokenType) bool {
+	return p.peekToken.Type == t
+}
+
+func (p *Parser) expectPeek(t token.TokenType) bool {
+	if p.peekTokenIs(t) {
+		p.nextToken()
+		return true
+	} else {
+		return false
+	}
+}
+
+func (p *Parser) registerPrefix(tokenType token.TokenType, fn prefixParseFn) {
+	p.prefixParseFns[tokenType] = fn
+}
+
+func (p *Parser) registerInfix(tokenType token.TokenType, fn infixParseFn) {
+	p.infixParseFns[tokenType] = fn
+}
