a5d7a9d92e64d43dd4272b8806191ccdd5a82f6d
g-hyoga <hyoga0216@gmail.com>
2018-10-21 02:15:57 +0900

    2.9まで

--- src/repl/repl.go
+++ src/repl/repl.go
@@ -31,6 +31,18 @@ func Start(in io.Reader, out io.Writer) {
 		l := lexer.New(line)
 		p := parser.New(l)
 		program := p.ParseProgram()
-		fmt.Printf("%+v\n", program.String())
+		if len(p.Errors()) != 0 {
+			printParserErrors(out, p.Errors())
+			continue
+		}
+
+		io.WriteString(out, program.String())
+		io.WriteString(out, "\n")
+	}
+}
+
+func printParserErrors(out io.Writer, errors []string) {
+	for _, msg := range errors {
+		io.WriteString(out, "\t"+msg+"\n")
 	}
 }
