d9a92a20dc8d264facf15e4f6e2539333d96dc80
g-hyoga <hyoga0216@gmail.com>
2018-10-23 20:44:03 +0900

    Add null, boolean

--- src/evaluator/evaluator.go
+++ src/evaluator/evaluator.go
@@ -5,6 +5,12 @@ import (
 	"github.com/g-hyoga/writing-interpreter-in-go/src/object"
 )
 
+var (
+	NULL  = &object.Null{}
+	TRUE  = &object.Boolean{Value: true}
+	FALSE = &object.Boolean{Value: false}
+)
+
 func Eval(node ast.Node) object.Object {
 	switch node := node.(type) {
 
@@ -17,6 +23,8 @@ func Eval(node ast.Node) object.Object {
 	// Expressions
 	case *ast.IntegerLiteral:
 		return &object.Integer{Value: node.Value}
+	case *ast.Boolean:
+		return nativeBoolToBooleanObject(node.Value)
 	}
 
 	return nil
@@ -28,6 +36,12 @@ func evalStatements(stmts []ast.Statement) object.Object {
 	for _, statement := range stmts {
 		result = Eval(statement)
 	}
-
 	return result
+}
+
+func nativeBoolToBooleanObject(input bool) *object.Boolean {
+	if input {
+		return TRUE
+	}
+	return FALSE
 }
--- src/repl/repl.go
+++ src/repl/repl.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"io"
 
+	"github.com/g-hyoga/writing-interpreter-in-go/src/evaluator"
 	"github.com/g-hyoga/writing-interpreter-in-go/src/lexer"
 	"github.com/g-hyoga/writing-interpreter-in-go/src/parser"
 )
@@ -36,8 +37,11 @@ func Start(in io.Reader, out io.Writer) {
 			continue
 		}
 
-		io.WriteString(out, program.String())
-		io.WriteString(out, "\n")
+		evaluated := evaluator.Eval(program)
+		if evaluated != nil {
+			io.WriteString(out, evaluated.Inspect())
+			io.WriteString(out, "\n")
+		}
 	}
 }
 
