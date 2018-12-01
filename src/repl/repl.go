825b300cf49a5b759e5ff6cfe7727fb2a1321614
g-hyoga <hyoga0216@gmail.com>
2018-12-01 11:45:47 +0900

    Add Let Statement

--- src/repl/repl.go
+++ src/repl/repl.go
@@ -7,6 +7,7 @@ import (
 
 	"github.com/g-hyoga/writing-interpreter-in-go/src/evaluator"
 	"github.com/g-hyoga/writing-interpreter-in-go/src/lexer"
+	"github.com/g-hyoga/writing-interpreter-in-go/src/object"
 	"github.com/g-hyoga/writing-interpreter-in-go/src/parser"
 )
 
@@ -14,6 +15,7 @@ const PROMPT = ">> "
 
 func Start(in io.Reader, out io.Writer) {
 	scanner := bufio.NewScanner(in)
+	env := object.NewEnvironment()
 
 	for {
 		fmt.Printf(PROMPT)
@@ -37,7 +39,7 @@ func Start(in io.Reader, out io.Writer) {
 			continue
 		}
 
-		evaluated := evaluator.Eval(program)
+		evaluated := evaluator.Eval(program, env)
 		if evaluated != nil {
 			io.WriteString(out, evaluated.Inspect())
 			io.WriteString(out, "\n")
