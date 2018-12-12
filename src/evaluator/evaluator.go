b8cf8b56f9570b7ec9bdd84467d7a4e0f03e1ddb
g-hyoga <hyoga0216@gmail.com>
2018-12-12 14:33:19 +0900

    Enable to eval array

--- src/evaluator/evaluator.go
+++ src/evaluator/evaluator.go
@@ -246,6 +246,27 @@ func evalPrefixExpression(operator string, right object.Object) object.Object {
 	}
 }
 
+func evalIndexExpression(left, index object.Object) object.Object {
+	switch {
+	case left.Type() == object.ARRAY_OBJ && index.Type() == object.INTEGER_OBJ:
+		return evalArrayIndexExpression(left, index)
+	default:
+		return newError("index operator not supported: %s", left.Type())
+	}
+}
+
+func evalArrayIndexExpression(array, index object.Object) object.Object {
+	arrayObject := array.(*object.Array)
+	idx := index.(*object.Integer).Value
+	max := int64(len(arrayObject.Elements) - 1)
+
+	if idx < 0 || idx > max {
+		return NULL
+	}
+
+	return arrayObject.Elements[idx]
+}
+
 func evalBlockStatement(block *ast.BlockStatement, env *object.Environment) object.Object {
 	var result object.Object
 
